934ba150d22319fc1ad2e14bb359663157684a19
svt1108 <svt_1108@inbox.ru>
2022-11-03 14:55:25 +0300

    feat: add buttons to header

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { CoreModule } from '../core/core.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, CoreModule],
+  imports: [CommonModule, CoreModule, SharedModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
