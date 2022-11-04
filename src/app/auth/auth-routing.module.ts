ce3e266b63c7adf3a5c056a6efd4764a9aa2c9d0
svt1108 <svt_1108@inbox.ru>
2022-11-04 23:07:10 +0300

    feat: change auth module

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegistrationComponent } from './pages/regisration/registration.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginPageComponent,
+    component: RegistrationComponent,
   },
 ];
 
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'primeng/api';
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegistrationComponent } from './pages/regisration/registration.component';
 import { ApiHelpersService } from './services/api-helpers.service';
 import { AuthComponent } from './components/auth/auth.component';
 import { ApiControlService } from './services/api-control.service';
+import { TranslocoRootModule } from '../transloco-root.module';
 
 @NgModule({
-  declarations: [LoginPageComponent, AuthComponent],
-  imports: [CommonModule, AuthRoutingModule, SharedModule],
+  declarations: [RegistrationComponent, AuthComponent],
+  imports: [CommonModule, AuthRoutingModule, SharedModule, TranslocoRootModule],
   providers: [ApiHelpersService, ApiControlService],
 })
 export class AuthModule {}
