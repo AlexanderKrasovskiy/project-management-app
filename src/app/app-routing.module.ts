b8bfa1dad0e0c1667cdaabc3211817745f8ba742
svt1108 <svt_1108@inbox.ru>
2022-11-06 17:47:12 +0300

    refactor: disable login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/components/login/login.component';
+// import { LoginComponent } from './auth/components/login/login.component';
 // import { RegComponent } from './auth/components/reg/reg.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
@@ -24,10 +24,10 @@ const routes: Routes = [
   //   path: 'registration',
   //   component: RegComponent,
   // },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
+  // {
+  //   path: 'login',
+  //   component: LoginComponent,
+  // },
 
   {
     path: 'boards',
