32c7485a8a051b460ae9aa2839a19b1a24d4faa0
Alla <regicter1810@mail.ru>
2022-11-06 22:47:15 +0300

    feat: add header for auth api

--- src/app/auth/services/api-helpers.service.ts
+++ src/app/auth/services/api-helpers.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, retry, catchError, EMPTY } from 'rxjs';
 import { MessageService } from 'primeng/api';
@@ -20,33 +20,45 @@ export class ApiHelpersService {
   ) {}
 
   register(payload: RegisterRequestModel): Observable<UserModel> {
-    return this.httpClient.post<UserModel>('/signup', payload).pipe(
-      retry(4),
-      catchError(() => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: 'Can not create user :(',
-          life: 5000,
-        });
-        // console.log('[ERROR]: ', error);
-        return EMPTY;
-      }),
-    );
+    const header = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.httpClient
+      .post<UserModel>('/signup', payload, {
+        headers: header,
+      })
+      .pipe(
+        retry(4),
+        catchError(() => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Can not create user :(',
+            life: 5000,
+          });
+          // console.log('[ERROR]: ', error);
+          return EMPTY;
+        }),
+      );
   }
 
   login(payload: LoginRequestModel): Observable<TokenResponseModel> {
-    return this.httpClient.post<TokenResponseModel>('/signin', payload).pipe(
-      retry(4),
-      catchError(() => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: 'Can not login :(',
-          life: 5000,
-        });
-        return EMPTY;
-      }),
-    );
+    const header = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.httpClient
+      .post<TokenResponseModel>('/signin', payload, {
+        headers: header,
+      })
+      .pipe(
+        retry(4),
+        catchError(() => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Can not login :(',
+            life: 5000,
+          });
+          return EMPTY;
+        }),
+      );
   }
 }
