b8557c173b23bb6726fdae6a0336bdf26656736a
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-11-08 16:16:59 +0600

    refactor: split details page into components

--- src/app/pages/details/details.module.ts
+++ src/app/pages/details/details.module.ts
@@ -6,9 +6,17 @@ import { SharedModule } from 'src/app/shared/shared.module';
 
 import { DetailsRoutingModule } from './details-routing.module';
 import { DetailsPageComponent } from './pages/details-page.component';
+import { BoardHeaderComponent } from './components/board-header/board-header.component';
+import { ColumnComponent } from './components/column/column.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
-  declarations: [DetailsPageComponent],
+  declarations: [
+    DetailsPageComponent,
+    BoardHeaderComponent,
+    ColumnComponent,
+    TaskComponent,
+  ],
   imports: [CommonModule, DetailsRoutingModule, DragDropModule, SharedModule],
 })
 export class DetailsModule {}
--- src/app/pages/details/pages/details-page.component.ts
+++ src/app/pages/details/pages/details-page.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-import {
-  CdkDragDrop,
-  moveItemInArray,
-  transferArrayItem,
-} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-details',
@@ -11,53 +7,17 @@ import {
   styleUrls: ['./details-page.component.scss'],
 })
 export class DetailsPageComponent {
-  displaySide = false;
-  sideBarStyles = {
-    width: '150px',
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    border: '2px solid rgba(0, 0, 0, 0.2)',
-    backdropFilter: 'blur(3px)',
-  };
-
   columns = [
     '1_Bronze age1_Bronze age1_Bronze age1_Bronze age',
     '2_Iron age',
     '3_Middle ages',
   ];
 
-  dropCols(event: CdkDragDrop<string[]>) {
+  dropCols(event: CdkDragDrop<string[]>, columns: any) {
     // console.log('EVENT: ', event);
     // console.log('DATA: ', event.item.data);
-    // console.log('BEFORE: ', this.columns);
-    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
-    // console.log('AFTER: ', this.columns);
-  }
-
-  isEditable = false;
-
-  toggleEdit() {
-    this.isEditable = !this.isEditable;
-  }
-
-  tasks = [
-    1, 2, 3, 4, 5, 6, 2, 3, 4, 5, 6, 2, 3, 4, 5, 6, 2, 3, 4, 5, 6, 2, 3, 4, 5,
-    6,
-  ];
-
-  dropTask(event: CdkDragDrop<number[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
+    // console.log('BEFORE: ', columns);
+    moveItemInArray(columns, event.previousIndex, event.currentIndex);
+    // console.log('AFTER: ', columns);
   }
 }
