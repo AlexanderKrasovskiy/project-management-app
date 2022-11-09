532c9b341f94c979b41bcbf44327d684d7ccde81
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-11-09 19:20:52 +0600

    feat: details - load board onInit, select columns, board title from store

--- src/app/pages/details/pages/details-page.component.ts
+++ src/app/pages/details/pages/details-page.component.ts
@@ -1,23 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { ActivatedRoute } from '@angular/router';
+import { map, Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { loadBoard } from 'src/app/store/actions/details.actions';
+import { selectColumns } from 'src/app/store/selectors/details.selectors';
+import { ColumnModel } from '../models/details.model';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details-page.component.html',
   styleUrls: ['./details-page.component.scss'],
 })
-export class DetailsPageComponent {
-  columns = [
-    '1_Bronze age1_Bronze age1_Bronze age1_Bronze age',
-    '2_Iron age',
-    '3_Middle ages',
-  ];
+export class DetailsPageComponent implements OnInit, OnDestroy {
+  subId$!: Subscription;
+  subCols$!: Subscription;
+  columns: ColumnModel[] = [];
 
-  dropCols(event: CdkDragDrop<string[]>, columns: any) {
+  constructor(private route: ActivatedRoute, private store: Store) {}
+
+  ngOnInit(): void {
+    this.subId$ = this.route.params
+      .pipe(map((params) => params['id']))
+      .subscribe((id: string) => this.store.dispatch(loadBoard({ id })));
+
+    // eslint-disable-next-line @ngrx/no-store-subscription
+    this.subCols$ = this.store.select(selectColumns).subscribe((cols) => {
+      this.columns = cols;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subId$.unsubscribe();
+    this.subCols$.unsubscribe();
+  }
+
+  dropCols(event: CdkDragDrop<string[]>) {
     // console.log('EVENT: ', event);
     // console.log('DATA: ', event.item.data);
     // console.log('BEFORE: ', columns);
-    moveItemInArray(columns, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
     // console.log('AFTER: ', columns);
   }
 }
