d3fbdb5251a7ed768227bd490661f56263dfbd7f
Alla <regicter1810@mail.ru>
2022-11-16 13:51:23 +0300

    feat: add api search service

---
+++ src/app/pages/search/services/api-search.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, EMPTY, Observable, retry } from 'rxjs';
+import {
+  BoardIDModel,
+  ColumnModel,
+  TaskModel,
+  UserRequestModel,
+} from '../../models/search.model';
+
+@Injectable()
+export class ApiSearchService {
+  constructor(private http: HttpClient) {}
+
+  getAllBoards(): Observable<BoardIDModel[]> {
+    return this.http.get<BoardIDModel[]>('/boards').pipe(
+      retry(4),
+      catchError((error) => {
+        console.log('[ERROR]: ', error);
+        return EMPTY;
+      }),
+    );
+  }
+
+  getAllColumns(id: string): Observable<ColumnModel[]> {
+    return this.http.get<ColumnModel[]>(`/boards/${id}/columns`).pipe(
+      retry(4),
+      catchError((error) => {
+        console.log('[ERROR]: ', error);
+        return EMPTY;
+      }),
+    );
+  }
+
+  getAllTasks(idBoard: string, idColumn: string): Observable<TaskModel[]> {
+    return this.http
+      .get<TaskModel[]>(`/boards/${idBoard}/columns/${idColumn}/tasks`)
+      .pipe(
+        retry(4),
+        catchError((error) => {
+          console.log('[ERROR]: ', error);
+          return EMPTY;
+        }),
+      );
+  }
+
+  getAllUsers(): Observable<UserRequestModel[]> {
+    return this.http.get<UserRequestModel[]>(`/users`).pipe(
+      retry(4),
+      catchError((error) => {
+        console.log('[ERROR]: ', error);
+        return EMPTY;
+      }),
+    );
+  }
+}
