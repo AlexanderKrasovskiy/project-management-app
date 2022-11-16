699607f63dbec94df8968a7127b7b11e738d6c25
svt1108 <svt_1108@inbox.ru>
2022-11-16 07:33:36 +0300

    feat: change modal window style

--- src/app/main/services/main.service.ts
+++ src/app/main/services/main.service.ts
@@ -45,7 +45,10 @@ export class MainService {
       title: '',
       description: '',
     };
-    const dialogRef = this.dialog.open(MainModalComponent, { data });
+    const dialogRef = this.dialog.open(MainModalComponent, {
+      data,
+      backdropClass: 'backdropBackground',
+    });
 
     dialogRef.afterClosed().subscribe((modalData) => {
       if (!modalData?.title || !modalData?.description) return;
