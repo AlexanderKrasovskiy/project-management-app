ac7680ddcfa765ca33f2c99151d2516a16f73c50
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-11-17 18:09:40 +0600

    refactor: remove sidebar from details page

--- src/app/pages/details/components/board-header/board-header.component.ts
+++ src/app/pages/details/components/board-header/board-header.component.ts
@@ -8,15 +8,6 @@ import { selectBoardTitle } from 'src/app/store/selectors/details.selectors';
   styleUrls: ['./board-header.component.scss'],
 })
 export class BoardHeaderComponent {
-  displaySide = false;
-
-  sideBarStyles = {
-    width: '150px',
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    border: '2px solid rgba(0, 0, 0, 0.2)',
-    backdropFilter: 'blur(3px)',
-  };
-
   boardTitle$ = this.store.select(selectBoardTitle);
 
   constructor(private store: Store) {}
