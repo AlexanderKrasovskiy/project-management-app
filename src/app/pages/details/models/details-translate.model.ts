a29d47919e66083b6ffc16b2bfcde846f61a2999
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-11-25 15:45:36 +0600

    feat: add translations enum to details/models

---
+++ src/app/pages/details/models/details-translate.model.ts
@@ -0,0 +1,12 @@
+export enum DetailsTranslations {
+  boardNotFound = 'details.boardNotFound',
+  columnNotFound = 'details.columnNotFound',
+  taskNotFound = 'details.taskNotFound',
+  userNotFound = 'details.userNotFound',
+  defaultError = 'details.defaultError',
+  errorTitle = 'details.errorTitle',
+  deleteTask = 'details.deleteTask',
+  editTask = 'details.editTask',
+  deleteColumn = 'details.deleteColumn',
+  createTask = 'details.createTask',
+}
